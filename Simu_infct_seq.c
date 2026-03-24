56f98d8a11fd469a9e6f950ccf0be6ec87e34dab
salasss <salasalkamabtr@gmail.com>
2026-03-24 14:29:37 +0100

    feat(simulation): update agent structure and implement movement and infection logic

--- Simu_infct_seq.c
+++ Simu_infct_seq.c
@@ -1,5 +1,6 @@
 #include <mpi.h>
 #include <stdio.h>
+#include <math.h>
 #include <stdlib.h>
 
 // 1st etape: def le struct
@@ -7,9 +8,9 @@ struct agent
 {
     char status;
     int time_in_status;
-    int dE;
-    int dI;
-    int dR;
+    double dE;
+    double dI;
+    double dR;
     int x;
     int y;
 };
@@ -38,13 +39,10 @@ int random_int_between(int A, int B)
 {
     return A + rand() % (B - A + 1);
 }
-double loi_exp()
-{
-}
 
-negExp(double inMean)
+double negExp(double inMean)
 {
-    return -inMean * log(1 - genrand_real2());
+    return -inMean * log(1 - rand() / (double)RAND_MAX);
 }
 int main(int argc, char *argv[])
 {
@@ -59,6 +57,7 @@ int main(int argc, char *argv[])
     int param_dE = 3;
     int param_dI = 7;
     int param_dR = 365;
+    int time = 25;
 
     // step2: init le monde
     struct agent *world = malloc(nbr_tot_agents * sizeof(struct agent));
@@ -78,8 +77,63 @@ int main(int argc, char *argv[])
         world[i].dR = negExp(param_dR);
         world[i].dI = negExp(param_dI);
     }
-    struct agent a;
-    set_status(&a, 'S');
+
+    // step3:deplacement et time
+    for (int i = 0; i < time; i++)
+    {
+        for (int j = 0; j < nbr_tot_agents; j++)
+        {
+            world[j].x = random_int_between(0, width_grille - 1);
+            world[j].y = random_int_between(0, Higth_grille - 1);
+        }
+
+        struct agent *world_inf = calloc(sizeof(struct agent), width_grille * width_grille);
+
+        for (int j = 0; j < nbr_tot_agents; j++)
+        {
+            int agent_x = world[j].x;
+            int agent_y = world[j].y;
+            if (world[j].status == 'S')
+            {
+                int Ni = 0;
+
+                int x0 = agent_x;
+                int y0 = agent_y;
+
+                int x_left = (x0 - 1 + width_grille) % width_grille;
+                int x_right = (x0 + 1) % width_grille;
+                int y_down = (y0 - 1 + Higth_grille) % Higth_grille;
+                int y_up = (y0 + 1) % Higth_grille;
+
+                for (int k = 0; k < nbr_tot_agents; k++)
+                {
+                    if (&world[j] == &world[k]) continue;
+                    if (world[k].status != 'I') continue;
+
+                    int xk = world[k].x;
+                    int yk = world[k].y;
+
+                    int voisin_x = (xk == x0) || (xk == x_left) || (xk == x_right);
+                    int voisin_y = (yk == y0) || (yk == y_down) || (yk == y_up);
+
+                    if (voisin_x && voisin_y)
+                    {
+                        Ni++;
+                    }
+                }
+
+                if (Ni > 0)
+                {
+                    double p = 1.0 - exp(-0.5 * Ni);
+                    double u = random_between(0.0, 1.0);
+                    if (u < p)
+                    {
+                        set_status(&world[j], 'E');
+                    }
+                }
+            }
+        }
+    }
 
     return 0;
 }
